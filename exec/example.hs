52ca71ce7a13b8bba9d89f57f3d68edc15a54d27
Malvin Gattinger <malvin@w4eg.de>
2022-07-18 17:53:31 +0200

    add T(Y)^I etc. in example, revealing an error in tiOf... functions

--- exec/example.hs
+++ exec/example.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Data.List
 import Data.String( IsString(..) )
 
 import Logic.PDL
@@ -28,7 +29,7 @@ main = do
   let ti = tiOf tWithInt
   ppTab ti
 
-  putStrLn "\nLowest M+ rule without interpolant:" -- TODO: actually, get the last/lowest!
+  putStrLn "\nLowest M+ rule without interpolant:"
   let Just mstart = lowestMplusWithoutIP ti
   ppTab mstart
 
@@ -37,9 +38,26 @@ main = do
   let tj = tjOf $ head $ childrenOf mstart
   ppTab tj
 
+  putStrLn "\nCompute T(Y) for all right components Y:"
+  putStrLn "List of all nodes:"
+  mapM_ (\pth -> do
+            putStr $ show pth ++ "\t\t"
+            putStrLn $ ppWFormsTyp Nothing (wfsOf (tj `at` pth)) []
+            )  $ allPathsIn tj
+  putStrLn "Definition 29:"
+  putStrLn "Y \t\tT(Y) \t\tT(Y)^ε \t\tT(Y)^I \t\tT(Y)^◁"
+  let rightComponents = nub $ map (\pth -> rightsOf (wfsOf (tj `at` pth)) ) (allPathsIn tj)
+  mapM_ (\y -> do
+            putStr $ toStrings y ++ if length (toStrings y) > 6 then "\t" else "\t\t"
+            putStr $ show (pathsInToNodeWith tj y) ++ "\t"
+            putStr $ show (tOfEpsilon tj y) ++ "\t\t"
+            putStr $ show (tOfI tj y) ++ "\t\t"
+            print (tOfTriangle tj y)
+           ) rightComponents
+
   -- T^K
   putStrLn "\nT^K (Def 27):"
-  let tk = tkOf $ tj
+  let tk = tkOf tj
   ppTab tk
 
   -- interpolant
