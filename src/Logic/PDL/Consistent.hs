a22bbb53450cbc0516bb3e679b2dade57159394e
Malvin Gattinger <malvin@w4eg.de>
2023-02-19 22:17:37 +0100

    add basic work-in-progress code for Logic.PDL.Consistent

---
+++ src/Logic/PDL/Consistent.hs
@@ -0,0 +1,81 @@
+module Logic.PDL.Consistent where
+
+import Data.Maybe
+
+import Logic.PDL
+import Logic.PDL.Prove.Tree
+
+-- | Given a tableaux, build a pointed Kripke model if possible.
+--
+-- TODO WARNING: copied from BasicModal - most rules still missing!
+--
+tabToMod :: Tableaux -> Maybe (Model [Form], [Form])
+-- no model when closed:
+tabToMod End = error "cannot make model for end marker"
+tabToMod (Node _ _ "✘" _ _) = Nothing
+-- same model as child for single-branch rules:
+tabToMod (Node _ _ "¬" _ [child]) = tabToMod child
+tabToMod (Node _ _ "∧" _ [child]) = tabToMod child
+-- splitting rule:
+tabToMod (Node _ _ "¬∧" _ children) =
+  listToMaybe $ mapMaybe tabToMod children
+-- atomic rule:
+tabToMod (Node wfs _ "¬☐" _ [child]) =
+  case tabToMod child of
+    Nothing -> Nothing
+    Just _  ->
+      let
+        childMs = -- we need open tableaus for all diamonds!
+          [ (aprog , tabToMod (tableauFor (Neg f : mapMaybe (projection aprog . fst . collapse) wfs) ) )
+          | Neg (Box (Ap aprog) f) <- map (fst . collapse) wfs ]
+      in
+        if all (isJust . snd) childMs
+          then Just $ comboModel
+                      (map (fst . collapse) wfs)
+                      [ prp | At prp <- map (fst . collapse) wfs ]
+                      (map (fmap fromJust) childMs)
+          else Nothing
+-- open leaf, create a single world model:
+tabToMod (Node wfs _ "" _ []) =
+  Just (KrM [(w, truthsHere)] noProgs, w) where
+  w = map (fst . collapse) wfs
+  noProgs = []
+  truthsHere = [ prp | At prp <- map (fst . collapse) wfs ]
+tabToMod (Node _ _ rule _ _) = error $ "unknown rule: " ++ rule
+
+mergeProg :: (Atom,[(a,a)]) -> [(Atom,[(a,a)])] -> [(Atom,[(a,a)])]
+mergeProg (_,_) [] = []
+mergeProg (pa,ra) ((pb,rb):rest)
+  | pa == pb  = (pa, ra ++ rb) : rest -- QUESTION: "nub" needed here?
+  | otherwise = (pb,rb) : mergeProg (pa,ra) rest
+
+comboProgs :: [(Atom,[(a,a)])] -> [(Atom,[(a,a)])]
+comboProgs [] = []
+comboProgs [ar] = [ar]
+comboProgs (ar:rest) = mergeProg ar $ comboProgs rest
+
+-- | Combine a single world and valuation with multiple pointed models.
+-- The new world is added "before" the previous points and becomes the new point.
+-- Preconditions:
+-- - the new world must be fresh, i.e. not be in the models already!
+-- - the given models must not overlap in worlds!
+-- Differences compared to BasicModal:
+-- - [ ] we now also need an atom for the program label.
+-- - [ ] when and how to create loops? we need them to stick to finite models.
+comboModel :: Eq w => w -> [Atom] -> [(Atom, (Model w, w))] -> (Model w, w)
+comboModel w localTruths oldModels = (newM, w) where
+  newM = KrM ((w, localTruths) : concatMap (worldsOf . fst . snd) oldModels) newProgs
+  oldLinks = concatMap (progsOf . fst . snd) oldModels
+  newLinks = [ (prg, [(w,w')]) | (prg,(_,w')) <- oldModels ]
+  newProgs = comboProgs (newLinks ++ oldLinks)
+
+toIntModel :: Eq a => (Model a, a) -> (Model Int, Int)
+toIntModel (KrM ws rl, oldActual) =
+  (KrM [ (k, v) | ((_, v), k) <- mapping ]
+       [ (prg, map myMapPair prel) | (prg, prel) <- rl ]
+  , myMap oldActual)
+  where
+    myMap old = fromJust (lookup old worldMap)
+    myMapPair (w, w') = (myMap w, myMap w')
+    mapping = zip ws [0..]
+    worldMap = zip (map fst ws) [0..]
